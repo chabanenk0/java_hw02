4700ad491ee4929404294b6442e0a5fe58822165
Dmytro Chabanenko <dchabanenko@corevalue.net>
2016-11-09 09:44:41 +0200

    Initial commit (crete project)

---
+++ src/main/java/com/github/chabanenk0/Main.java
@@ -0,0 +1,11 @@
+package com.github.chabanenk0;
+
+/**
+ * Main class for inheritance homework
+ * Created by dchabanenko on 09.11.16.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/github/chabanenk0/MainTest.java
@@ -0,0 +1,17 @@
+package com.github.chabanenk0;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dchabanenko on 09.11.16.
+ */
+public class MainTest {
+    @Test
+    public void mainTest()
+    {
+        Main main = new Main();
+        assertNotNull(main);
+    }
+}
